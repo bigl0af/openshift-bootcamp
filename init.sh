0c8171e0bbb59fdcc04412e953157de9f865d422
David Fox <david.fox2@netapp.com>
2018-03-26 09:00:59 -0400

    working bootcamp code

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+OSWD=$HOME/openshift-ansible-clone
 
+curl -k -s https://c1-gitlab.corp.netapp.com/ansible/ansible-pull/raw/master/bootstrap.sh | ssh-agent /bin/bash
+
+yum install git ansible python-paramiko sshpass python-crypto python2-crypto ansible -y
+git clone https://github.com/openshift/openshift-ansible.git $OSWD && cd $OSWD
+git checkout release-3.6
+ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
